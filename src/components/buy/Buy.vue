7aa5c3eaf64a759e5989e23ada8589c654e71b70
Vicky Koblinski <vickykoblinski@gmail.com>
2018-01-23 10:25:30 -0500

    Dummy Buy data and buy/Item styling

--- src/components/buy/Buy.vue
+++ src/components/buy/Buy.vue
@@ -1,11 +1,32 @@
 <!-- Buy component -->
 
 <template>
-  <h1>Buy</h1>
+  <div class="row">
+    <app-item v-for="item in items" :key="item.id"></app-item>
+  </div>
 </template>
 
 <script>
-export default {};
+import Item from "./Item.vue";
+
+/** Temporary data for developing with */
+const dummyData = [
+  { id: 1, name: "Golden Sword", price: 110 },
+  { id: 2, name: "Carrot", price: 52 },
+  { id: 3, name: "Plain Boots", price: 50 },
+  { id: 4, name: "Bear Fur", price: 134 }
+];
+
+export default {
+  data() {
+    return {
+      items: dummyData
+    };
+  },
+  components: {
+    appItem: Item
+  }
+};
 </script>
 
 <style scoped>
--- src/components/buy/Item.vue
+++ src/components/buy/Item.vue
@@ -1,7 +1,21 @@
 <!-- buy/Item component -->
 
 <template>
-  <h1>buy/Item</h1>
+<div class="col-sm-6">
+  <div class="card border-success mb-3" style="max-width: 18rem;">
+    <div class="card-header">NAME
+      <small>(Price: PRICE)</small>
+    </div>
+    <div class="card-body text-success">
+      <div class="input-group mb-3">
+        <input type="number" class="form-control" placeholder="Quantity">
+        <div class="input-group-append">
+          <button class="btn btn-success" type="button">Button</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
