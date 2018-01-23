f28f11d6f1c103a08d47685727e4a5c0ebc8d7e8
Vicky Koblinski <koblinski@etsu.edu>
2018-01-23 09:01:05 -0500

    Hooks up VueRouter

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+// Wires up our router to global Vue object
+Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
